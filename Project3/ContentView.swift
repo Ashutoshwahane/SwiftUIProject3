f12660a38b57da0f4909f3e83d513ff6dc991b09
Ashutoshwahane <ashutosh.wahane10@gmail.com>
2020-08-18 13:11:09 +0530

    frame and predefine text

--- Project3/ContentView.swift
+++ Project3/ContentView.swift
@@ -9,8 +9,47 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var useRedText = true
+    
+    let myName = Text("Ashutosh Wahane")
+    var internetName : some View{
+    Text("Willskhlaifa")
+    }
+    
     var body: some View {
-        Text("Hello, World!")
+        
+        
+        VStack{
+            myName.foregroundColor(Color.red).font(.headline)
+            internetName.foregroundColor(Color.blue)
+                .font(.title)
+        }
+            
+        
+//
+//        Button("Namaste Ashutosh"){
+//        // do nothing
+//            print(type(of: self.body))
+//        }
+//            .frame(width: 200, height: 200)
+//            .background(Color.red)
+//
+//        if self.useRedText{
+//            return Text("Hello Ashutosh")
+//                .background(Color.blue)
+//        }else{
+//            return Text("Hello Ashutosh")
+//                .background(Color.red)
+//        }
+            
+        
+        
+        
+//        Text("Hello, World!")
+//            .frame(maxWidth: .infinity, maxHeight: .infinity)
+//            .background(Color.red)
+//            .edgesIgnoringSafeArea(.all)
     }
 }
 
