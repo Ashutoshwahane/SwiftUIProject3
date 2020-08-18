5880ef4c6b1a44c86e927d9803f7ae3c6c124084
Ashutoshwahane <ashutosh.wahane10@gmail.com>
2020-08-18 17:46:59 +0530

    Stepper in SwiftUi

--- Project3/ContentView.swift
+++ Project3/ContentView.swift
@@ -9,56 +9,21 @@
 import SwiftUI
 
 
-struct Title: ViewModifier{
-    func body(content: Content) -> some View {
-        content
-            .font(.largeTitle)
-            .foregroundColor(.white)
-        .padding()
-            .background(Color.blue)
-        .clipShape(RoundedRectangle(cornerRadius: 10))
-    }
-}
-
-struct WaterMark: ViewModifier {
-    
-    var text : String
-    func body(content: Content) -> some View {
-       ZStack(alignment: .bottomTrailing) {
-            content
-            Text(text)
-                .font(.caption)
-                .foregroundColor(.white)
-                .padding(5)
-                .background(Color.black)
-        }
-    }
-}
-
-
-
-extension View{
-    func titleText() -> some View{
-        return modifier(Title())
-    }
-    func waterMark(with text: String) -> some View{
-        return modifier(WaterMark(text: text))
-    }
-}
-
 struct ContentView: View {
+    @State private var sleepAmount = 8.0
     
     var body: some View {
     
-        Color.blue
-            .frame(width: 200, height: 200)
-        .waterMark(with: "Hi Ashutosh")
-        
+        Stepper(value: $sleepAmount, in: 4...12, step: 0.25){
+            Text("\(sleepAmount, specifier: "%g") hours")
+        }
+     
     }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
     }
-    }
+}
+    
 }
