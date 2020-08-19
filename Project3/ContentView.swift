4173b1386e7be4e52d750d0b46be075f5366176a
Ashutoshwahane <ashutosh.wahane10@gmail.com>
2020-08-19 21:24:59 +0530

    UI design for BetterRest

--- Project3/ContentView.swift
+++ Project3/ContentView.swift
@@ -11,17 +11,54 @@ import SwiftUI
 
 struct ContentView: View {
     @State private var wakeUp = Date()
+    @State private var sleepAmount = 8.0
+    @State private var coffeAmount = 1
     
     var body: some View {
-    
-        DatePicker("please enter the date", selection: $wakeUp, in: Date()...)
-        .labelsHidden()
+        NavigationView{
+            VStack{
+                Text("When you want to wake up")
+                    .font(.headline)
+                
+                DatePicker("Please Enter time",
+                           selection: $wakeUp,
+                           displayedComponents: .hourAndMinute
+                            ).labelsHidden()
+                
+                Text("Desired amount of sleep")
+                    .font(.headline)
+                Stepper(value: $sleepAmount, in: 4...12, step: 0.25){
+                    Text("\(sleepAmount, specifier: "%g") hours")
+                }
+                
+                Text("Daily Coffe intake")
+                    .font(.headline)
+                Stepper(value: $coffeAmount, in: 1...20){
+                    if coffeAmount == 1 {
+                        Text("1 cups")
+                    }else{
+                        Text("\(coffeAmount) cups")
+                    }
+                }
+            }
+            
+            .navigationBarTitle("BetterRest")
+            .navigationBarItems(trailing:
+                Button(action: calculateBedTime){
+                    Text("Calculate")
+                }
+            )
+        }
     }
-
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
+    func calculateBedTime(){
+        
     }
+        
 }
-    
+
+    struct ContentView_Previews: PreviewProvider {
+        static var previews: some View {
+        ContentView()
+            
+        }
 }
